cee578c25d765c9935a90dd6db9c03cebae83d21
Andrew Matsumura <42308941+glader0293@users.noreply.github.com>
2025-06-24 22:38:50 +0000

    Final version

--- src/models.py
+++ src/models.py
@@ -12,31 +12,37 @@ class User(db.Model):
     username: Mapped[str] = mapped_column(String(50))
     email: Mapped[str] = mapped_column(String(100))
 
-    favorites = relationship("Favorite", back_populates="user", cascade="all, delete-orphan")
+    favorite_characters = relationship("FavoriteCharacter", back_populates="user", cascade="all, delete-orphan")
+    favorite_planets = relationship("FavoritePlanet", back_populates="user",cascade="all, delete-orphan")
 
 class Character(db.Model):
     __tablename__ = 'character'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100))
 
+    favorited_by = relationship("FavoriteCharacter", back_populates="character", cascade="all, delete-orphan")
+
 class Planet(db.Model):
     __tablename__ = 'planet'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100))
 
-class Favorite(db.Model):
-    __tablename__ = 'favorite'
+    favorited_by = relationship("FavoritePlanet", back_populates="planet", cascade="all, delete-orphan")
+
+class FavoriteCharacter(db.Model):
+    __tablename__ = 'favorite_character'
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
-    item_id: Mapped[int] = mapped_column(Integer, nullable=False)
-    item_type: Mapped[str] = mapped_column(String(50), nullable=False) 
+    character_id: Mapped[int] =mapped_column(ForeignKey('character.id'))
 
-    user = relationship("User", back_populates="favorites")
+    user = relationship("User", back_populates="favorite_characters")
+    character = relationship("Character", back_populates="favorited_by")
 
-    @property
-    def item(self):
-        if self.item_type == 'character':
-            return db.session.get(Character, self.item_id)
-        elif self.item_type == 'planet':
-            return db.session.get(Planet, self.item_id)
-        return None
+class FavoritePlanet(db.Model):
+    __tablename__ = 'favorite_planet'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id')) #links the table object with the name of the table
+    planet_id: Mapped[int] =mapped_column(ForeignKey('planet.id')) #links the table object with the name of the table
+
+    user = relationship("User", back_populates="favorite_planets")
+    planet = relationship("Planet", back_populates="favorited_by")
